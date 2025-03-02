abfdc34bfcea3ab69957e4c37f7938be71d1b105
giftontoolkit <giftontoolkit@gmail.com>
2025-03-02 08:14:04 +0500

    Add files via upload

--- v2/functions.php
+++ v2/functions.php
@@ -1,9 +1,9 @@
 <?php
 // Enqueue Styles using mys2.css located in theme directory
-function gifton_enqueue_assets_css() {
+function myTM_enqueue_assets_css() {
     // Enqueue mys2.css
-    wp_enqueue_style('gifton-mys2-style', get_template_directory_uri() . '/mys2.css', array(), '1.0.0', 'all');
+    wp_enqueue_style('myTM-mys2-style', get_template_directory_uri() . '/mys2.css', array(), '1.0.0', 'all');
 }
-add_action('wp_enqueue_scripts', 'gifton_enqueue_assets_css');
+add_action('wp_enqueue_scripts', 'myTM_enqueue_assets_css');
 
 ?>
