14c8985d288159eb438902ce4ae15a21e754e523
giftontoolkit <giftontoolkit@gmail.com>
2025-03-02 08:07:42 +0500

    Add files via upload

--- v1/functions.php
+++ v1/functions.php
@@ -1,9 +1,8 @@
 <?php
 // Enqueue Styles using style.css located in theme directory
-function gifton_enqueue_assets_css() {
+function myTM_enqueue_assets_css() {
     // Enqueue style.css
-    wp_enqueue_style('gifton-style', get_stylesheet_uri());
+    wp_enqueue_style('myTM-style', get_stylesheet_uri());
 }
-add_action('wp_enqueue_scripts', 'gifton_enqueue_assets_css');
-
+add_action('wp_enqueue_scripts', 'myTM_enqueue_assets_css');
 ?>
---
+++ v1/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php bloginfo('name'); ?></title>
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+
+    <h1>Heading styling by using <b>style.css</b> file located in root directory</h1>
+    <h2 class="myH2">Heading 2 styling by using external classes</h2>
+    <p>We got blogname by using <b>bloginfo</b> function. This blog name is <span style="color: red;"> <?php bloginfo('name')?> </span>This is set by using</p>
+    
+</body>
+</html>
